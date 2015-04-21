70c522ee1d68d05e1b42f80fa0aa88f9903d222a
weishidavip@163.com <weishidavip@163.com>
2015-04-21 14:41:29 +0800

    add config

--- go/init.go
+++ go/init.go
@@ -1,7 +1,9 @@
 package client
 
 import (
+	"fmt"
 	"github.com/hprose/hprose-go/hprose"
+	"gopkg.in/ini.v1"
 )
 
 type Wherer struct {
@@ -19,9 +21,40 @@ type Sorter struct {
 }
 
 func init() {
+	workDir, err := WorkDir()
+	if err != nil {
+		panic(err)
+	}
+	customPath := path.Join(workDir, "custom/conf/rpc.ini")
+	cfg, err = ini.Load(customPath)
+	if err != nil {
+		panic(err)
+	}
+
+	server := cfg.Section("").Key("server").MustString("http://127.0.0.1:3000/")
+
+	fmt.Println("连接rpc服务器 :", server)
+
 	// c := hprose.NewClient("tcp://127.0.0.1:3456/")
-	c := hprose.NewClient("http://127.0.0.1:3000/")
+	c := hprose.NewClient(server)
 	c.UseService(&UserClient)
 	c.UseService(&StrategyStockClient)
 	c.UseService(&StrategyUserClient)
+}
+
+func WorkDir() (string, error) {
+	execPath, err := ExecPath()
+	return path.Dir(strings.Replace(execPath, "\\", "/", -1)), err
+}
+
+func ExecPath() (string, error) {
+	file, err := exec.LookPath(os.Args[0])
+	if err != nil {
+		return "", err
+	}
+	p, err := filepath.Abs(file)
+	if err != nil {
+		return "", err
+	}
+	return p, nil
 }
