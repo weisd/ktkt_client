e62b52a8017da18532143bea12c91f3ff2d31fa8
weishidavip@163.com <weishidavip@163.com>
2015-05-06 12:56:54 +0800

    strategy oid 改为int64类型

--- go/strategy.go
+++ go/strategy.go
@@ -13,7 +13,7 @@ const (
 
 type Strategy struct {
 	Id        int64     `json:"id"`
-	Oid       string    `json:"oid"`
+	Oid       int64     `json:"oid"`
 	Title     string    `json:"title"`
 	Keywords  string    `json:"keywords"`
 	Type      int       `json:"type"`
